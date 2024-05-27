43be247f37fc60878c555e53d672865c0ae99ffd
Kartik <codewithme.kartik@gmail.com>
2024-05-27 22:43:34 +0530

    Server set up

---
+++ src/main/java/com/kartik/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.kartik.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="fjwliaefhnaiuhefoiwehfklwiuaeflihaneflkihnbaleihfbnlihanf";
+    public static final String JWT_HEADER="Authorization";
+}
